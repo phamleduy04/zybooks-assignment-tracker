6e1cfd70f4f9fbce0b6dd6d01e24c5e5e56c4164
Duy Pham Le <32657584+phamleduy04@users.noreply.github.com>
2023-03-05 20:18:49 -0600

    add support to more database
    mongodb, redis, sqlite, postgres, mysql, etcd :tada:

--- src/Database/index.ts
+++ src/Database/index.ts
@@ -2,7 +2,7 @@ import Keyv from 'keyv';
 import * as log from '../Log';
 
 // if not stated mongodb url, will use memory as database
-const db = new Keyv(process.env.MONGODB);
+const db = new Keyv(process.env.DATABASE_URL);
 
 db.on('error', (err) => log.err(err));
 
